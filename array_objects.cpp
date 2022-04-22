40cc63c2322cf9e4b7c5f971d611665fb1ff031e
Suraj <13surajsingh01@gmail.com>
2022-04-22 08:19:57 +0530

    Formating

--- array_objects.cpp
+++ array_objects.cpp
@@ -1,25 +1,30 @@
 #include <iostream>
 using namespace std;
 
-class employee{
+class employee
+{
     int ID;
     int Salary;
-    public:
-    void SetId(void){
-        Salary=122;
-        cout<<"Enter the ID of the Employee "<<endl;
-        cin>>ID;
-    }
-    void GetId(void){
-        cout<<"The ID of this Employee "<<ID<<endl;
 
+public:
+    void SetId(void)
+    {
+        Salary = 122;
+        cout << "Enter the ID of the Employee " << endl;
+        cin >> ID;
+    }
+    void GetId(void)
+    {
+        cout << "The ID of this Employee " << ID << endl;
     }
 };
 int main()
 {
     employee fb[4];
-    for(int i=0; i<4; i++){
-    fb[i].SetId();
-    fb[i].GetId();}
+    for (int i = 0; i < 4; i++)
+    {
+        fb[i].SetId();
+        fb[i].GetId();
+    }
     return 0;
 }
--- friend_function.cpp
+++ friend_function.cpp
@@ -1,37 +1,39 @@
 #include <iostream>
 using namespace std;
 
-class complex{
-    int a,b;
-    public:
-    void setData(int n1,int n2){
+class complex
+{
+    int a, b;
+
+public:
+    void setData(int n1, int n2)
+    {
         a = n1;
         b = n2;
-
     }
     friend complex sumComplex(complex o1, complex o2);
-    void printNumber(){
-        cout<<"Your number is "<<a<<"+"<<b<<"i"<<endl;
-
+    void printNumber()
+    {
+        cout << "Your number is " << a << "+" << b << "i" << endl;
     }
 };
 
-complex sumComplex(complex o1, complex o2){
+complex sumComplex(complex o1, complex o2)
+{
     complex o3;
     o3.setData((o1.a + o2.a), (o1.b + o2.b));
     return o3;
 }
 int main()
 {
-    complex c1,c2,sum;
-    c1.setData(1,4);
+    complex c1, c2, sum;
+    c1.setData(1, 4);
     c1.printNumber();
-    c2.setData(5,8);
+    c2.setData(5, 8);
     c2.printNumber();
 
-    sum = sumComplex(c1,c2);
+    sum = sumComplex(c1, c2);
     sum.printNumber();
 
-    
     return 0;
 }
