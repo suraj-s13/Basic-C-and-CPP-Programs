5490f8a042b02d00ac006c05df7b1a0f5f2a9a72
Suraj <13surajsingh01@gmail.com>
2022-04-22 08:17:26 +0530

    Some Canges

--- array_basic.cpp
+++ array_basic.cpp
@@ -1,21 +1,21 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 int main()
 {
     int n;
-    cout<<"Enter Size:";
-    cin>>n;
+    cout << "Enter Size:";
+    cin >> n;
     int A[n];
-    //int A[6]={2,4,6,8,10,12};
-    for(int x:A)
+    // int A[6]={2,4,6,8,10,12};
+    for (int x : A)
     {
-        cout<<x<<endl;
+        cout << x << endl;
     }
-    //for(int i=0;i<10;i++)
+    // for(int i=0;i<10;i++)
     //{
-    //cout<<A[i]<<endl;
-    //}
-   
+    // cout<<A[i]<<endl;
+    // }
+
     return 0;
-} 
+}
--- function_basics.cpp
+++ function_basics.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
-int add(int a, int b){
+int add(int a, int b)
+{
     int c;
-    c=a+b;
-    return(c);
+    c = a + b;
+    return (c);
 }
 int main()
-{ 
-    int x,y,z;
-    x=10;
-    y=5;
-    z=add(x,y);
+{
+    int x, y, z;
+    x = 10;
+    y = 5;
+    z = add(x, y);
     printf("sum is %d", z);
 
-return 0;
-} 
+    return 0;
+}
